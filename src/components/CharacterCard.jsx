1b4feaccbd9d4ccdf960cceb4626dd804c4dec72
elena.alcaraz.martin@gmail.com <elena.alcaraz.martin@gmail.com>
2024-05-24 10:21:52 +0200

    More style

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -7,7 +7,12 @@ function CharacterCard({ character }) {
 
         <Link className="character_card_link" to={`/detail/${character.id}`}>
           <img src={character.image} alt="" />
-          <h2 className="character_card_title">{character.name}</h2>
+          <h2 className="character_card_title">
+          {character.status === "Dead" ? 
+              <i className="fa-solid fa-skull-crossbones character_card_deadIcon"></i> 
+              : "" } 
+            {character.name}
+          </h2>
         </Link>
 
         <h4 className="character_card_specie">{character.specie}</h4>
